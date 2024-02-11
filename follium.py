fa04f129e9da92c31327e10fca1debf6bd3f2afe
notnotmelon <zacharypicco@gmail.com>
2024-02-11 03:47:41 -0600

    Update agricultural-land.csv and follium.py

--- follium.py
+++ follium.py
@@ -159,12 +159,12 @@ def create_map_2(columns, df, year, primary_key):
     return m
 
 def create_map(filename, year, primary_key):
-    # schema = parse_schemas.get_schema()
-    # columns = schema[filename][0]
-    df = pd.read_csv(filename)
-    return create_map_2(df, year, primary_key)
+    schema = parse_schemas.get_schema()
+    columns = schema[filename][0]
+    df = pd.read_csv(f'Backend/CSV/{filename}.csv')
+    return create_map_2(columns, df, year, primary_key)
 
 if __name__ == '__main__':
-    m = create_map('agricultural-land', 2020, 'Agricultural land ')
+    m = create_map('agricultural-land', 2020, 'Agricultural land | 00006610 || Area | 005110 || hectares')
     m.save('index.html')
     webbrowser.open('index.html')
