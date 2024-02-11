8d40969602099391f976d10e564b5f8f888d08d2
notnotmelon <zacharypicco@gmail.com>
2024-02-10 21:55:38 -0600

    Add random import and search functionality

--- follium.py
+++ follium.py
@@ -4,6 +4,7 @@ import folium
 import webbrowser
 from folium.plugins import *
 import json
+import random
 
 night_layer = folium.TileLayer(
     tiles='https://map1.vis.earthdata.nasa.gov/wmts-webmerc/VIIRS_CityLights_2012/default/{time}/{tilematrixset}{maxZoom}/{z}/{y}/{x}.{format}',
@@ -26,6 +27,7 @@ m = folium.Map(
     location=(45.5236, -122.6750),
     tiles=tile_layer,
 )
+
 
 button_html = """
 <div style="position: fixed; top: 10px; left: 10px; z-index: 1000;">
@@ -63,7 +65,6 @@ folium.Circle(
     popup='Water Amount: 100000'
 ).add_to(m)
 
-import random
 
 # Generate random heatmap data
 heatmap_data = []
@@ -77,9 +78,6 @@ for _ in range(5000):
 heatmap = HeatMap(heatmap_data, radius=20)
 heatmap.add_to(m)
 
-m.save("index.html")
-webbrowser.open("index.html")
-
 # Create HeatMap layer and add it to the map
 heatmap = HeatMap(heatmap_data)
 heatmap.add_to(m)
@@ -87,14 +85,22 @@ heatmap.add_to(m)
 with open('datasets/world-countries.json') as handle:
     country_geo = json.loads(handle.read())
 
-for i in country_geo['features']:
-    if i['properties']['name'] == 'Germany':
-        country = i
-        break
+country_layer = folium.FeatureGroup(name='Countries')
+country_layer.add_to(m)
 
-folium.GeoJson(country, name='germany').add_to(m)
+for feature in country_geo['features']:
+    folium.GeoJson(feature).add_to(country_layer)
 
-folium.LayerControl().add_to(m)
+search_control = Search(
+    layer=country_layer,
+    geom_type='Polygon',
+    placeholder='Search for a country',
+    collapsed=False,
+    search_label='name',
+    search_zoom=6,
+    position='topright'
+)
+search_control.add_to(m)
 
 m.save("index.html")
 webbrowser.open("index.html")
